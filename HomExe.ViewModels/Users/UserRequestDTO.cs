1c8fa01db27d9359035e3320248969169b986e2a
Hoài Nam <namhoaidoan15@gmail.com>
2022-10-15 13:11:49 +0700

    Edit put controller

---
+++ HomExe.ViewModels/Users/UserRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomExe.ViewModels.Users
+{
+    public class UserRequestDTO
+    {
+        public int UserId { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public decimal? Phone { get; set; }
+        public string? Height { get; set; }
+        public string? Weight { get; set; }
+        public string? Status { get; set; }
+    }
+}
